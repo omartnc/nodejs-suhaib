30335fba713704184e22a6818e689a719ed242b4
omartnc <omer_tunc_04@hotmail.com>
2018-11-01 00:53:11 +0300

    entercounter doysaya sildim

--- models/entercounter.js
+++ models/entercounter.js
@@ -2,32 +2,3 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 const { schemaMember } = require("./member");
 const { schemaDoor } = require("./door");
-
-const schemaEnterCounter = new mongoose.Schema({
-  member: {
-    type: schemaMember,
-    required: true
-  },
-  door: {
-    type: schemaDoor,
-    required: true
-  },
-  Date: {
-    type: Date,
-    required: false,
-    default: Date.now()
-  }
-});
-const EnterCounter = mongoose.model("EnterCounter", schemaEnterCounter);
-
-function validateEnterCounter(EnterCounter) {
-  const schema = {
-    doorId: Joi.objectId().required(),
-    memberId: Joi.objectId().required()
-  };
-  return Joi.validate(EnterCounter, schema);
-}
-
-exports.EnterCounter = EnterCounter;
-exports.schemaEnterCounter = schemaEnterCounter;
-exports.validate = validateEnterCounter;
